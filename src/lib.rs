a0da81bf3781762327883ef4a3b6fcd7fd33e5db
yangfeng <yangfengzzz@hotmail.com>
2021-05-31 13:46:24 +0800

    delete files

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,3 @@
  * // personal capacity and am not conveying any rights to any intellectual
  * // property of any third parties.
  */
-
-pub mod entity;
-pub mod layer;
-pub mod component;
-pub mod bounding_box;
-pub mod vector3;
-pub mod bounding_sphere;
-pub mod point3;
-pub mod scene;
-pub mod engine;
-pub mod engine_object;
