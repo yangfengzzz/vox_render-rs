6f017cbecb566a347743ffe590d73b7cfcae62da
yangfeng <yangfengzzz@hotmail.com>
2021-06-05 14:24:22 +0800

    rewrite simd_quad

--- src/simd_quaternion.rs
+++ src/simd_quaternion.rs
@@ -96,71 +96,67 @@ impl SimdQuaternion {
         return quat.normalize();
     }
 
-    // // Returns the quaternion that will rotate vector _from into vector _to,
-    // // around their plan perpendicular axis. The input vectors must be normalized.
-    // #[inline]
-    // pub fn from_unit_vectors(_from: SimdFloat4,
-    //                          _to: SimdFloat4) -> SimdQuaternion {
-    //     unsafe {
-    //         // http://lolengine.net/blog/2014/02/24/quaternion-from-two-vectors-final
-    //         debug_assert!(simd_int4::are_all_true1(
-    //             simd_int4::and(simd_float4::is_normalized_est3(_from), simd_float4::is_normalized_est3(_to))) &&
-    //             "Input vectors must be normalized.".parse().unwrap());
-    //
-    //         let real_part = _mm_add_ps(simd_float4::x_axis(), simd_float4::dot3(_from, _to));
-    //         if simd_float4::get_x(real_part) < 1.0e-6 {
-    //             // If _from and _to are exactly opposite, rotate 180 degrees around an
-    //             // arbitrary orthogonal axis.
-    //             // Normalization isn't needed, as from is already.
-    //             let mut from: [f32; 4] = [0.0; 4];
-    //             simd_float4::store_ptr_u(_from, &mut from);
-    //             let quat = SimdQuaternion {
-    //                 xyzw:
-    //                 match f32::abs(from[0]) > f32::abs(from[2]) {
-    //                     true => simd_float4::load(-from[1], from[0], 0.0, 0.0),
-    //                     false => simd_float4::load(0.0, -from[2], from[1], 0.0)
-    //                 }
-    //             };
-    //             return quat;
-    //         } else {
-    //             // This is the general code path.
-    //             let quat = SimdQuaternion { xyzw: simd_float4::set_w(simd_float4::cross3(_from, _to), real_part) };
-    //             return normalize(&quat);
-    //         }
-    //     }
-    // }
+    // Returns the quaternion that will rotate vector _from into vector _to,
+    // around their plan perpendicular axis. The input vectors must be normalized.
+    #[inline]
+    pub fn from_unit_vectors(_from: SimdFloat4,
+                             _to: SimdFloat4) -> SimdQuaternion {
+        // http://lolengine.net/blog/2014/02/24/quaternion-from-two-vectors-final
+        debug_assert!(SimdInt4::are_all_true1(
+            &SimdInt4::and(&SimdFloat4::is_normalized_est3(&_from), SimdFloat4::is_normalized_est3(&_to))) &&
+            "Input vectors must be normalized.".parse().unwrap());
+
+        let real_part = SimdFloat4::x_axis() + _from.dot3(_to);
+        return if real_part.get_x() < 1.0e-6 {
+            // If _from and _to are exactly opposite, rotate 180 degrees around an
+            // arbitrary orthogonal axis.
+            // Normalization isn't needed, as from is already.
+            let mut from: [f32; 4] = [0.0; 4];
+            _from.store_ptr_u(&mut from);
+            let quat = SimdQuaternion {
+                xyzw:
+                match f32::abs(from[0]) > f32::abs(from[2]) {
+                    true => SimdFloat4::load(-from[1], from[0], 0.0, 0.0),
+                    false => SimdFloat4::load(0.0, -from[2], from[1], 0.0)
+                }
+            };
+            quat
+        } else {
+            // This is the general code path.
+            let mut result = _from.cross3(_to);
+            result.set_w(real_part);
+            let quat = SimdQuaternion { xyzw: result };
+            quat.normalize()
+        };
+    }
 }
 
-// impl Mul for SimdQuaternion {
-//     type Output = SimdQuaternion;
-//     #[inline]
-//     fn mul(self, rhs: Self) -> Self::Output {
-//         // Original quaternion multiplication can be swizzled in a simd friendly way
-//         // if w is negated, and some w multiplications parts (1st/last) are swaped.
-//         //
-//         //        p1            p2            p3            p4
-//         //    _a.w * _b.x + _a.x * _b.w + _a.y * _b.z - _a.z * _b.y
-//         //    _a.w * _b.y + _a.y * _b.w + _a.z * _b.x - _a.x * _b.z
-//         //    _a.w * _b.z + _a.z * _b.w + _a.x * _b.y - _a.y * _b.x
-//         //    _a.w * _b.w - _a.x * _b.x - _a.y * _b.y - _a.z * _b.z
-//         // ... becomes ->
-//         //    _a.w * _b.x + _a.x * _b.w + _a.y * _b.z - _a.z * _b.y
-//         //    _a.w * _b.y + _a.y * _b.w + _a.z * _b.x - _a.x * _b.z
-//         //    _a.w * _b.z + _a.z * _b.w + _a.x * _b.y - _a.y * _b.x
-//         // - (_a.z * _b.z + _a.x * _b.x + _a.y * _b.y - _a.w * _b.w)
-//         unsafe {
-//             let p1 =
-//                 _mm_mul_ps(simd_float4::swizzle3332(self.xyzw), simd_float4::swizzle0122(rhs.xyzw));
-//             let p2 =
-//                 _mm_mul_ps(simd_float4::swizzle0120(self.xyzw), simd_float4::swizzle3330(rhs.xyzw));
-//             let p13 =
-//                 simd_float4::madd(simd_float4::swizzle1201(self.xyzw), simd_float4::swizzle2011(rhs.xyzw), p1);
-//             let p24 =
-//                 simd_float4::madd(simd_float4::swizzle2013(self.xyzw), simd_float4::swizzle1203(rhs.xyzw), p2);
-//             return SimdQuaternion { xyzw: simd_float4::xor_fi(_mm_add_ps(p13, p24), simd_int4::mask_sign_w()) };
-//         }
-//     }
-// }
+impl Mul for SimdQuaternion {
+    type Output = SimdQuaternion;
+    #[inline]
+    fn mul(self, rhs: Self) -> Self::Output {
+        // Original quaternion multiplication can be swizzled in a simd friendly way
+        // if w is negated, and some w multiplications parts (1st/last) are swaped.
+        //
+        //        p1            p2            p3            p4
+        //    _a.w * _b.x + _a.x * _b.w + _a.y * _b.z - _a.z * _b.y
+        //    _a.w * _b.y + _a.y * _b.w + _a.z * _b.x - _a.x * _b.z
+        //    _a.w * _b.z + _a.z * _b.w + _a.x * _b.y - _a.y * _b.x
+        //    _a.w * _b.w - _a.x * _b.x - _a.y * _b.y - _a.z * _b.z
+        // ... becomes ->
+        //    _a.w * _b.x + _a.x * _b.w + _a.y * _b.z - _a.z * _b.y
+        //    _a.w * _b.y + _a.y * _b.w + _a.z * _b.x - _a.x * _b.z
+        //    _a.w * _b.z + _a.z * _b.w + _a.x * _b.y - _a.y * _b.x
+        // - (_a.z * _b.z + _a.x * _b.x + _a.y * _b.y - _a.w * _b.w)
+        let p1 =
+            self.xyzw.swizzle3332() * rhs.xyzw.swizzle0122();
+        let p2 =
+            self.xyzw.swizzle0120() * rhs.xyzw.swizzle3330();
+        let p13 = self.xyzw.swizzle1201().madd(rhs.xyzw.swizzle2011(), p1);
+        let p24 = self.xyzw.swizzle2013().madd(rhs.xyzw.swizzle1203(), p2);
+        return SimdQuaternion { xyzw: (p13 + p24).xor_fi(SimdInt4::mask_sign_w()) };
+    }
+}
 
 impl Neg for SimdQuaternion {
     type Output = SimdQuaternion;
@@ -223,39 +219,39 @@ impl SimdQuaternion {
     pub fn is_normalized_est(&self) -> SimdInt4 {
         return self.xyzw.is_normalized_est4();
     }
-}
 
-// // Returns to an axis angle representation of quaternion _q.
-// // Assumes quaternion _q is normalized.
-// #[inline]
-// pub fn to_axis_angle(_q: &SimdQuaternion) -> SimdFloat4 {
-//     unsafe {
-//         debug_assert!(simd_int4::are_all_true1(simd_float4::is_normalized_est4(_q.xyzw)) && "_q is not normalized.".parse().unwrap());
-//         let x_axis = simd_float4::x_axis();
-//         let clamped_w = simd_float4::clamp(_mm_sub_ps(_mm_setzero_ps(), x_axis), simd_float4::splat_w(_q.xyzw), x_axis);
-//         let half_angle = simd_float4::acos_x(clamped_w);
-//
-//         // Assuming quaternion is normalized then s always positive.
-//         let s = simd_float4::splat_x(simd_float4::sqrt_x(simd_float4::nmadd(clamped_w, clamped_w, x_axis)));
-//         // If s is close to zero then direction of axis is not important.
-//         let low = simd_float4::cmp_lt(s, simd_float4::load1(1e-3));
-//         return simd_float4::select(low, x_axis,
-//                                    simd_float4::set_w(_mm_mul_ps(_q.xyzw, simd_float4::rcp_est_nr(s)), _mm_add_ps(half_angle, half_angle)));
-//     }
-// }
-//
-// // Computes the transformation of a Quaternion and a vector _v.
-// // This is equivalent to carrying out the quaternion multiplications:
-// // _q.conjugate() * (*this) * _q
-// // w component of the returned vector is undefined.
-// #[inline]
-// pub fn transform_vector(_q: &SimdQuaternion,
-//                         _v: SimdFloat4) -> SimdFloat4 {
-//     // http://www.neil.dantam.name/note/dantam-quaternion.pdf
-//     // _v + 2.f * cross(_q.xyz, cross(_q.xyz, _v) + _q.w * _v)
-//     let cross1 = simd_float4::madd(simd_float4::splat_w(_q.xyzw), _v, simd_float4::cross3(_q.xyzw, _v));
-//     let cross2 = simd_float4::cross3(_q.xyzw, cross1);
-//     unsafe {
-//         return _mm_add_ps(_mm_add_ps(_v, cross2), cross2);
-//     }
-// }
+    // Returns to an axis angle representation of quaternion _q.
+    // Assumes quaternion _q is normalized.
+    #[inline]
+    pub fn to_axis_angle(&self) -> SimdFloat4 {
+        unsafe {
+            debug_assert!(self.xyzw.is_normalized_est4().are_all_true1() && "self is not normalized.".parse().unwrap());
+            let x_axis = SimdFloat4::x_axis();
+            let clamped_w = SimdFloat4::clamp(&-x_axis, self.xyzw.splat_w(), x_axis);
+            let half_angle = clamped_w.acos_x();
+
+            // Assuming quaternion is normalized then s always positive.
+            let s = SimdFloat4::nmadd(&clamped_w, clamped_w, x_axis).sqrt_x().splat_x();
+            // If s is close to zero then direction of axis is not important.
+            let low = SimdFloat4::cmp_lt(&s, SimdFloat4::load1(1e-3));
+
+            let mut result = self.xyzw * s.rcp_est_nr();
+            result.set_w(half_angle + half_angle);
+            return SimdFloat4::select(low, x_axis, result);
+        }
+    }
+
+    // Computes the transformation of a Quaternion and a vector _v.
+    // This is equivalent to carrying out the quaternion multiplications:
+    // _q.conjugate() * (*this) * _q
+    // w component of the returned vector is undefined.
+    #[inline]
+    pub fn transform_vector(&self,
+                            _v: SimdFloat4) -> SimdFloat4 {
+        // http://www.neil.dantam.name/note/dantam-quaternion.pdf
+        // _v + 2.f * cross(_q.xyz, cross(_q.xyz, _v) + _q.w * _v)
+        let cross1 = self.xyzw.splat_w().madd(_v, self.xyzw.cross3(_v));
+        let cross2 = self.xyzw.cross3(cross1);
+        return _v + cross2 + cross2;
+    }
+}
