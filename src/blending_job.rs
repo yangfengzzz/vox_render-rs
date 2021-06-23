94ba12416dc7ab2c8e876e3130a9b029b6034d0b
yangfeng <yangfengzzz@hotmail.com>
2021-06-23 16:31:35 +0800

    add BlendingJob

--- src/blending_job.rs
+++ src/blending_job.rs
@@ -113,7 +113,35 @@ impl<'a> BlendingJob<'a> {
     // smaller than the bind pose buffer.
     // -if the threshold value is less than or equal to 0.f.
     pub fn validate(&self) -> bool {
-        todo!()
+        // Don't need any early out, as jobs are valid in most of the performance
+        // critical cases.
+        // Tests are written in multiple lines in order to avoid branches.
+        let mut valid = true;
+
+        // Test for valid threshold).
+        valid &= self.threshold > 0.0;
+
+        // Test for nullptr begin pointers.
+        // Blending layers are mandatory, additive aren't.
+        valid &= !self.bind_pose.is_empty();
+        valid &= !self.output.is_empty();
+
+        // The bind pose size defines the ranges of transforms to blend, so all
+        // other buffers should be bigger.
+        let min_range = self.bind_pose.len();
+        valid &= self.output.len() >= min_range;
+
+        // Validates layers.
+        for layer in self.layers {
+            valid &= validate_layer(layer, min_range);
+        }
+
+        // Validates additive layers.
+        for layer in self.additive_layers {
+            valid &= validate_layer(layer, min_range);
+        }
+
+        return valid;
     }
 
     // Runs job's blending task.
@@ -121,6 +149,111 @@ impl<'a> BlendingJob<'a> {
     // more details.
     // Returns false if *this job is not valid.
     pub fn run(&mut self) -> bool {
-        todo!()
+        if !self.validate() {
+            return false;
+        }
+
+        // Initializes blended parameters that are exchanged across blend stages.
+        let mut process_args = ProcessArgs::new(self);
+
+        // Blends all layers to the job output buffers.
+        blend_layers(&mut process_args);
+
+        // Applies bind pose.
+        blend_bind_pose(&mut process_args);
+
+        // Normalizes output.
+        normalize(&mut process_args);
+
+        // Process additive blending.
+        add_layers(&mut process_args);
+
+        return true;
     }
+}
+
+fn validate_layer(_layer: &Layer, _min_range: usize) -> bool {
+    let mut valid = true;
+
+    // Tests transforms validity.
+    valid &= _layer.transform.len() >= _min_range;
+
+    // Joint weights are optional.
+    if !_layer.joint_weights.is_empty() {
+        valid &= _layer.joint_weights.len() >= _min_range;
+    } else {
+        valid &= _layer.joint_weights.is_empty();
+    }
+    return valid;
+}
+
+// Defines parameters that are passed through blending stages.
+struct ProcessArgs<'a> {
+    // Allocates enough space to store a accumulated weights per-joint.
+    // It will be initialized by the first pass processed, if any.
+    // This is quite big for a stack allocation (4 byte * maximum number of
+    // joints). This is one of the reasons why the number of joints is limited
+    // by the API.
+    // Note that this array is used with SoA data.
+    // This is the first argument in order to avoid wasting too much space with
+    // alignment padding.
+    accumulated_weights: [SimdFloat4; crate::skeleton::Constants::KMaxSoAJoints as usize],
+
+    // The job to process.
+    job: &'a BlendingJob<'a>,
+
+    // The number of transforms to process as defined by the size of the bind
+    // pose.
+    num_soa_joints: usize,
+
+    // Number of processed blended passes (excluding passes with a weight <= 0.f),
+    // including partial passes.
+    num_passes: i32,
+
+    // Number of processed partial blending passes (aka with a weight per-joint).
+    num_partial_passes: i32,
+
+    // The accumulated weight of all layers.
+    accumulated_weight: f32,
+}
+
+impl<'a> ProcessArgs<'a> {
+    fn new(_job: &'a BlendingJob<'a>) -> ProcessArgs<'a> {
+        let args = ProcessArgs {
+            accumulated_weights: [SimdFloat4::zero(); crate::skeleton::Constants::KMaxSoAJoints as usize],
+            job: _job,
+            num_soa_joints: _job.bind_pose.len(),
+            num_passes: 0,
+            num_partial_passes: 0,
+            accumulated_weight: 0.0,
+        };
+        // The range of all buffers has already been validated.
+        debug_assert!(args.job.output.len() >= args.num_soa_joints);
+        debug_assert!(args.accumulated_weights.len() >= args.num_soa_joints);
+        return args;
+    }
+}
+
+// Blends all layers of the job to its output.
+fn blend_layers(_args: &mut ProcessArgs) {
+    todo!()
+}
+
+// Blends bind pose to the output if accumulated weight is less than the
+// threshold value.
+fn blend_bind_pose(_args: &mut ProcessArgs) {
+    todo!()
+}
+
+// Normalizes output rotations. Quaternion length cannot be zero as opposed
+// quaternions have been fixed up during blending passes.
+// Translations and scales are already normalized because weights were
+// pre-multiplied by the normalization ratio.
+fn normalize(_args: &mut ProcessArgs) {
+    todo!()
+}
+
+// Process additive blending pass.
+fn add_layers(_args: &mut ProcessArgs) {
+    todo!()
 }
